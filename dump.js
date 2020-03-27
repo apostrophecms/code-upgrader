73b3f19056051145f56724e32dd36b77f62e2705
Alex Bea <abea@mailbox.org>
2020-03-27 14:30:16 -0500

    Adds linting and cleans up

--- dump.js
+++ dump.js
@@ -1,6 +1,6 @@
 const moduleName = process.argv[2];
 const code = require('fs').readFileSync(moduleName, 'utf8');
 const acorn = require('acorn');
-const escodegen = require('escodegen');
 const parsed = acorn.parse(code);
-console.log(JSON.stringify(parsed, null, '  '));
+
+console.info(JSON.stringify(parsed, null, '  '));
