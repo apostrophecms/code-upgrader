669a0c293a6b5a429bde39e02c7148270e1e6931
Thomas Boutell <tom@apostrophecms.com>
2021-11-19 07:21:27 -0500

    eslint

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const argv = require('boring')();
 const cp = require('child_process');
 const linter = require('./lib/linter');
 const upgrader = require('./lib/upgrader');
-const { stripIndent } = require('common-tags');
 
 if (argv._[0] === 'reset') {
   cp.execSync('git reset --hard && git clean -df');
