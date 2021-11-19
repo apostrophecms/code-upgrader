aaea16d2c03711bdefa8ca7eb56bb17f0d0bc7ae
Thomas Boutell <tom@apostrophecms.com>
2021-11-19 07:18:25 -0500

    pr concerns re naming and communication

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const argv = require('boring')();
 const cp = require('child_process');
 const linter = require('./lib/linter');
-const converter = require('./lib/converter');
+const upgrader = require('./lib/upgrader');
+const { stripIndent } = require('common-tags');
 
 if (argv._[0] === 'reset') {
   cp.execSync('git reset --hard && git clean -df');
 } else if (argv._[0] === 'lint') {
   linter();
-} else if (argv._[0] === 'convert') {
-  converter();
+} else if (argv._[0] === 'upgrade') {
+  upgrader();
 } else {
   // help goes here
   console.log('halp');
